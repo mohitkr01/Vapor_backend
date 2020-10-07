c8b31178e2ed98d8b0224d7b856b2c70d5fa121a
Mohit Kumar <mohit.kumar@curtisfitch.com>
2020-10-07 15:00:45 +0530

    urlencoded

--- server.js
+++ server.js
@@ -94,8 +94,12 @@ application = require('./application'),
     }
   });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+
 app.use(express.static('uploads'));
 app.use(express.static('Vapor_Backend'));
 
