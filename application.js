9c870ce3916a56c942bbe12c3003e3cbbcb6dd29
Mohit Kumar <mohit.kumar@curtisfitch.com>
2020-10-07 16:07:28 +0530

    Logo

--- application.js
+++ application.js
@@ -186,7 +186,7 @@ const express = require('express'),
                 // Imagepath= newpath.split("/")
                 // console.log("image path",Imagepath);
                 // // console.log(req.file.originalname);
-                // let imagepath = 'https://vaporbackend.herokuapp.com/'+Imagepath[1]
+                // let imagepath = 'Localhost:8000/'+Imagepath[1]
                
                 db.collection('Business_detail').updateMany({userId:new MongoClient.ObjectID(req.body.userId)},
                 {
@@ -194,7 +194,7 @@ const express = require('express'),
                     // "Upload_Logo": req.file.originalname,
                     // "Logopath":imagepath
                     $set: {
-                      "Signature": req.body.Signature
+                      "logo": req.body.logo
                     }
                   }
                 },{
@@ -282,7 +282,7 @@ const express = require('express'),
                     res.end();
                     throw err;
                   }
-                  res.send(result[0].Signature);
+                  res.send(result[0].logo);
                 })
               },
 
